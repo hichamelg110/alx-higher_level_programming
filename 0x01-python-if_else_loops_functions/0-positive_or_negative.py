1fcb3c15aaaeb97cede7a94a05a3b6d5141acce1
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-02 16:44:52 -0800

    task

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-c = random.randint(-10, 10)
-if c > 0:
-    print(f"{c} is positive")
-elif c == 0:
-    print(f"{c} is zero")
-else:
-    print(f"{c} is negative")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-for m in range(ord('a'), ord('z') + 1):
-    if chr(m) != 'e' and chr(m) != 'q':
-        print('{:c}'.format(m), end='')
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+    if letter not in 'qe':
+        print('{}'.format(letter), end='')
