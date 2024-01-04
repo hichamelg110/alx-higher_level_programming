b99859082367825f002a8875db734d38a0ad9387
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-04 11:33:30 -0800

    task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,2 +1,8 @@
-from add_0.py import add
-add(1, 2)
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
--- 0x02-python-import_modules/add_0.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    print("a + b")
