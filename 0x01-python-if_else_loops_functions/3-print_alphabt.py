b269509e7e07396de29f0a1e545b4fc81ef39d05
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-04 06:13:52 -0800

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for letter in 'abcdefghijklmnopqrstuvwxyz':
-    if letter not in 'qe':
-        print('{}'.format(letter), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e':
+        continue
+    elif chr(i) == 'q':
+        continue
+    print(chr(i), end='')
