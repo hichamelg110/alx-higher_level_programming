23f97e4380f3e494652bb7e5813447d6cb27c6b0
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-08 14:12:16 -0800

    task

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    res_list = []
+    if my_list:
+        for num in my_list:
+            res_list.append(False if num % 2 else True)
+
+    return res_list
+
