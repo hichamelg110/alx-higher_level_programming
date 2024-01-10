5a49a426c582d815481fa77390564303084252f6
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-10 11:40:17 -0800

    task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = []
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
+    unique_set = set(my_list)
+    return sum(unique_set)
