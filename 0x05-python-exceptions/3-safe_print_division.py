825e084641085e887aa5c9732600501e38674bf0
hichamelg110 <elisejuneaileen@gmail.com>
2024-01-22 15:17:09 -0800

    task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     result = None
     try:
-        result = a/b
+        result = a / b
     except ZeroDivisionError:
         return None
     finally:
-        print("{}".format(result))
+        print("Inside result: {}".format(result))
         return result
