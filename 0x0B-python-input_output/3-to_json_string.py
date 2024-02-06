a860eac6085631c2f1cc11ceb5e31b82ec5b202a
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-06 09:09:40 -0800

    task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """JSON serialization utility function."""
 
+
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)."""
     json_use = json.dumps(my_obj)
