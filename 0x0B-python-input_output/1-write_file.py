184b1ab1064ec8acc655ca192ea7c498ed1447fe
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-06 08:22:06 -0800

    task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""File-writing utility function."""
+
+
+def write_file(filename="", text=""):
+    """Writes the provided text to a UTF8 text file with the given filename."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
