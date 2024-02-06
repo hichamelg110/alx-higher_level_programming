403f4ab2532106b42823279c45163a50494a099f
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-06 12:07:24 -0800

    task

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,15 +5,17 @@ class Student:
     """Defines a student by first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with
-        first name, last name, and age.
+        """Initializes a Student instance with.
+        Args:
+            first_name: first name of the student.
+            last_name: last name of the student.
+            age: age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation
-        of a Student instance.
-        """
+        """Retrieves a dictionary representation of a Student instance."""
         return self.__dict__
+
