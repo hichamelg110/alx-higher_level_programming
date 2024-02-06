038ee0212acb7b2029bfe9069a9065951895cbd4
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-06 07:04:04 -0800

    task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""File-reading utility function."""
+
+def read_file(filename=""):
+    """Reads and prints the contents of a UTF8 text file to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
