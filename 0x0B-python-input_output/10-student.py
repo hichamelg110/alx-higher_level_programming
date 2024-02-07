4db6387d01cc277fd4e03b33add3fc089ef89d23
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-07 12:43:13 -0800

    task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,7 +5,8 @@ class Student:
     """Defines a student by first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first name, last name, and age."""
+        """Initializes a Student instance with first name,
+        last name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -13,8 +14,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes contained in this list are retrieved.
-        Otherwise, all attributes are retrieved.
         """
         if attrs is None:
             return {attr: getattr(self, attr) for attr in vars(self)}
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Student class definition with selective attribute retrieval and reloading from JSON."""
+"""Student class definition with selective
+attribute retrieval and reloading from JSON."""
+
 
 class Student:
     """Defines a student by first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student with first name, last name, and age."""
+        """Initializes a new Student with first name,
+        last name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -13,8 +16,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes contained in this list are retrieved.
-        Otherwise, all attributes are retrieved.
         """
         if attrs is not None:
             return {attr: getattr(self, attr) for attr in attrs if attr in vars(self)}
@@ -26,4 +27,3 @@ class Student:
         for key, value in json.items():
             if hasattr(self, key):
                 setattr(self, key, value)
-
