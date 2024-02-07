c0e18eba2ef8decc18f4e5dc5c9967f4efb26e7a
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-07 07:22:12 -0800

    task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends the provided text to a UTF8 text file with the given filename."""
+    """Appends the provided text to UTF8 text file with the given filename."""
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
