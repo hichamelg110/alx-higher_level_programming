ec74497b1be59217c4af1e2a64363735d5e8e0c4
hichamelg110 <elisejuneaileen@gmail.com>
2024-04-22 09:14:21 -0700

    task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 
 const myVar0 = 'Python is cool';
 const myVar1 = 'C is fun';
-const myVar2 = "JavaScript is amazing";
+const myVar2 = 'JavaScript is amazing';
 console.log(myVar0);
 console.log(myVar1);
 console.log(myVar2);
