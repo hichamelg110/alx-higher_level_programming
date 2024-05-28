fd8fe2ac8306c62f2837c618eec0242e93a9a2e1
hichamelg110 <elisejuneaileen@gmail.com>
2024-05-28 10:26:14 -0700

    task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const reader = require('fs');
 
-reader.readFile(process.argv[2], 'utf8', function (err, content) {
-  if (error) {
-    console.error('Error reading file:', error);
-  } else {
-    process.stdout.write(content);
-  }
+reader.readFile(process.argv[2], 'utf8', function (err, content)
+{
+if (error)
+{
+console.error('Error reading file:', error);
+}
+else
+{
+process.stdout.write(content);
+}
 });
